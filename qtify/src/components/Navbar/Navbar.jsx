04452a6f08db4bdabd17986e915db4e7c9115abe
Anshuman Sarkar <55441@hexaware.com>
2025-11-26 15:08:57 +0530

    home page

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,30 @@
+// src/components/Navbar/Navbar.jsx
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from "../Button/Button";
 import styles from "./Navbar.module.css";
 import Logo from "../Logo/Logo";
 import Search from "../Search/Search";
+import Button from "../Button/Button";
 
 function Navbar({ searchData }) {
   return (
     <nav className={styles.navbar}>
-      <Link to="/">
-        <Logo />
-      </Link>
-      <Search
-        placeholder="Search a song of your choice"
-        searchData={searchData}
-      />
-      <Button>Give Feedback</Button>
+      <div className={styles.left}>
+        <Link to="/">
+          <Logo />
+        </Link>
+      </div>
+
+      <div className={styles.center}>
+        <Search
+          placeholder="Search a song of your choice"
+          searchData={searchData}
+        />
+      </div>
+
+      <div className={styles.right}>
+        <Button>Give Feedback</Button>
+      </div>
     </nav>
   );
 }
